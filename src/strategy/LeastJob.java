4159cf687eb7f1fd6a8257501bdc5b4ad346096a
bsh6463 <bsh9510@gmail.com>
2021-11-08 22:30:59 +0900

    211108 Strategy Pattern

---
+++ src/strategy/LeastJob.java
@@ -0,0 +1,13 @@
+package strategy;
+
+public class LeastJob implements Scheduler{
+    @Override
+    public void getNextCall() {
+        System.out.println("상담 전화를 순서대로 대기열에서 가져옴");
+    }
+
+    @Override
+    public void sendCallToAgent() {
+        System.out.println("현재 상담이 없거나 대기가 적은 상담원에게 할당");
+    }
+}
---
+++ src/strategy/Scheduler.java
@@ -0,0 +1,7 @@
+package strategy;
+
+public interface Scheduler {
+
+    public void getNextCall();
+    public void sendCallToAgent();
+}
