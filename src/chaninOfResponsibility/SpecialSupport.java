f8e524f76f8ada2413ac7a9f90b83296c4a12118
bsh6463 <bsh9510@gmail.com>
2021-11-15 00:07:57 +0900

    211114 Chain Of Responsibility

---
+++ src/chaninOfResponsibility/SpecialSupport.java
@@ -0,0 +1,21 @@
+package chaninOfResponsibility;
+
+public class SpecialSupport extends Support{
+
+    public int number;
+
+    public SpecialSupport(String name, int number) {
+        super(name);
+        this.number = number;
+    }
+
+    @Override
+    protected boolean resolve(Trouble trouble) {
+        if (trouble.getNumber() == number){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
---
+++ src/chaninOfResponsibility/Trouble.java
@@ -0,0 +1,18 @@
+package chaninOfResponsibility;
+
+public class Trouble {
+
+    int number;
+
+    public Trouble(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
